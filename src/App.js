e793af1852f6390bcf6a8988bbc035b61c8d9262
Thomas Sankara <thomassankara74@gmail.com>
2021-08-10 15:38:19 +0300

    component complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import background from "./images/illustration-hero.svg"
+import music from "./images/icon-music.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className="showcase">
+        <div className="background-image"></div>
+        <div className="card">
+          <img src={background} alt="hero pattern" className="hero" />
+          <h1>Order summary</h1>
+          <p>
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+
+          <div className="pricing">
+            <div>
+              <img src={music} alt="music button" title="Play music" />
+
+              <article>
+                <h3>Annual plan</h3>
+                <p>$59.99/year</p>
+              </article>
+            </div>
+
+            <button className="btn btn-change">Change</button>
+          </div>
+
+          <div className="buttons">
+            <button className="btn btn-primary">Proceed to Payment</button>
+            <button className="btn btn-secondary">Cancel Order</button>
+          </div>
+        </div>
+
+        <div class="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noopenner noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://tsbsankara.co.ke"
+            target="_blank"
+            rel="noopenner noreferrer"
+          >
+            Thomas Sankara
+          </a>
+          .
+        </div>
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
